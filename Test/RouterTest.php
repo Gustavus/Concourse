b38015dcd664e4fcdb049db4cf369eb8d29485ea
Billy Visto <bvisto@gustavus.edu>
2013-01-11 09:22:47 -0600

    Made use of the authentication mocking and wrote more tests

--- Test/RouterTest.php
+++ Test/RouterTest.php
@@ -86,9 +86,11 @@ class RouterTest extends Test
    */
   public function handleRequestSecure()
   {
-    $this->routingConfig['/indexTwo/{id}']['visibleTo'] = array('ProjectName' => [Gatekeeper::PERMISSION_ALL]);
+    $this->authenticate('bvisto');
+    $this->routingConfig['/indexTwo/{id}']['visibleTo'] = array('ProjectName', [Gatekeeper::PERMISSION_ALL]);
     $actual = Router::handleRequest($this->routingConfig, '/indexTwo/23');
     $this->assertSame('RouterTestController indexTwo(23)', $actual);
+    $this->unAuthenticate();
   }
 
   /**
@@ -138,9 +140,52 @@ class RouterTest extends Test
    */
   public function userCanAccessPage()
   {
-    $this->routingConfig['/indexTwo/{id}']['visibleTo'] = ['studentOrgs' => [Gatekeeper::PERMISSION_ANONYMOUS]];
-    Gatekeeper::setUsername('bvisto');
+    $this->routingConfig['/indexTwo/{id}']['visibleTo'] = [
+        'studentOrgs',
+        [Gatekeeper::PERMISSION_ANONYMOUS],
+    ];
+
+    $this->authenticate('bvisto');
     $actual = $this->call('\Gustavus\Concourse\Router', 'userCanAccessPage', array($this->routingConfig['/indexTwo/{id}']));
     $this->assertTrue($actual);
+    $this->unAuthenticate();
+  }
+
+  /**
+   * @test
+   */
+  public function userCanAccessPageLoginLevel()
+  {
+    $this->routingConfig['/indexTwo/{id}']['visibleTo'] = [
+        'studentOrgs',
+        [Gatekeeper::PERMISSION_ANONYMOUS],
+        'loginLevel' => Gatekeeper::LOG_IN_LEVEL_ALL
+    ];
+
+    $this->authenticate('bvisto');
+    $actual = $this->call('\Gustavus\Concourse\Router', 'userCanAccessPage', array($this->routingConfig['/indexTwo/{id}']));
+    $this->assertTrue($actual);
+    $this->unAuthenticate();
+  }
+
+  /**
+   * @test
+   */
+  public function userCanAccessPageCallbacks()
+  {
+    $this->routingConfig['/indexTwo/{id}']['visibleTo'] = [
+        'studentOrgs',
+        [Gatekeeper::PERMISSION_ANONYMOUS, 'callbacks' => [[$this, 'someCallback']]],
+    ];
+
+    $this->authenticate('bvisto');
+    $actual = $this->call('\Gustavus\Concourse\Router', 'userCanAccessPage', array($this->routingConfig['/indexTwo/{id}']));
+    $this->assertFalse($actual);
+    $this->unAuthenticate();
+  }
+
+  public function someCallback()
+  {
+    return false;
   }
 }
